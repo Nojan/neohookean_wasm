a0fb1eec1d2f574b90289d8a05e5c84c3e18f6f6
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2021-10-09 22:15:52 -0400

    vecSetZero

--- src/neohookean.c
+++ src/neohookean.c
@@ -1,6 +1,14 @@
 //clang src/neohookean.c --target=wasm32-unknown-unknown-wasm --optimize=3 -nostdlib -Wl,--export-all -Wl,--no-entry -Wl,--allow-undefined --output html/neohookean.wasm
 
 __attribute__((used)) 
+void vecSetZero(float* a, int anr) {
+    anr *= 3;
+    a[anr++] = 0.f;
+    a[anr++] = 0.f;
+    a[anr]   = 0.f;
+}
+
+__attribute__((used)) 
 void vecCopy(float* a, int anr, float* b, int bnr) {
     anr *= 3; bnr *= 3;
     a[anr++] = b[bnr++]; 
